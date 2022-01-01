7b876aa9af0f0b608f9da4f3bbc17479fc4c85b4
Marouane Bouaricha <marouane.tchiko@gmail.com>
2022-01-01 21:12:05 +0100

    added a merge function

--- main.go
+++ main.go
@@ -3,5 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Merge...")
+	fmt.Println(merge([]int{1, 3, 6}, []int{2, 4, 5}))
+}
+
+func merge(Left, Right []int) []int {
+	A := make([]int, len(Left)+len(Right))
+	i, j, k := 0, 0, 0
+	// compare left & right side elements before merging
+	for i < len(Left) && j < len(Right) {
+		if Left[i] < Right[j] {
+			A[k] = Left[i]
+			i++
+		} else {
+			A[k] = Right[j]
+			j++
+		}
+		k++
+	}
+	// check if any elements from the left/right side
+	// were missed in the comparison section above
+
+	for i < len(Left) {
+		A[k] = Left[i]
+		i++
+		k++
+	}
+	for j < len(Right) {
+		A[k] = Right[j]
+		j++
+		k++
+	}
+
+	return A
 }
