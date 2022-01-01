eb705b4d637eddba7463cd3ddf99e245a326cbe8
Marouane Bouaricha <marouane.tchiko@gmail.com>
2022-01-01 21:34:36 +0100

    "improve call"

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MarouaneBouaricha/merge-sort
+
+go 1.13
--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(merge([]int{1, 3, 6}, []int{2, 4, 5}))
+	fmt.Println(sort([]int{6, 1, 0, 2, 4, 3, 7, 5}, func(a, b int) bool {
+		return a < b
+	}))
 }
 
-func merge(Left, Right []int) []int {
+type sortFunc func(a, b int) bool
+
+func merge(Left, Right []int, fn sortFunc) []int {
 	A := make([]int, len(Left)+len(Right))
 	i, j, k := 0, 0, 0
 	// compare left & right side elements before merging
 	for i < len(Left) && j < len(Right) {
-		if Left[i] < Right[j] {
+		if fn(Left[i], Right[j]) {
 			A[k] = Left[i]
 			i++
 		} else {
@@ -34,5 +38,25 @@ func merge(Left, Right []int) []int {
 		k++
 	}
 
+	return A
+}
+
+func sort(A []int, fn sortFunc) []int {
+	if fn == nil {
+		fn = func(a, b int) bool {
+			return a < b
+		}
+	}
+	if len(A) > 1 {
+		m := len(A) / 2
+		Left := A[:m]
+		Right := A[m:]
+
+		// sort the right side
+		// sort the left side
+		// merge the sorted sides
+		// do this recursively till the slice has 1 element only
+		A = merge(sort(Right, fn), sort(Left, fn), fn)
+	}
 	return A
 }
